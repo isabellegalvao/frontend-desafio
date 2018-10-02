c4f9e85235a58d7695aa662e0841f14ca16c45a2
Isabelle Galvão <isabelle@upecommerce.com.br>
2018-10-02 14:07:08 -0300

    update comments

--- script/rep.js
+++ script/rep.js
@@ -1,27 +1,37 @@
-const getRepDetail = localStorage.repositoryUri;
+(function() {
 
-$.ajax({
-    url: getRepDetail,
-    dataType: "json",
-    complete: function (data) {
-        const json = data.responseJSON;
+    "use strict";
 
-        if (json.message == "Not Found") {
-            console.log("Repositório não encontrado")
-        } else {
-            console.log(json);
+    const getRepDetail = localStorage.repositoryUri;
+    const boxRep = $(".repository")
 
-            const name = json.name;
-            const description = json.description;
-            const stars = json.stargazers_count;
-            const language = json.language;
-            const link = json.svn_url;
 
-            $("[data-js-name]").html(name)
-            $("[data-js-desc]").html(description)
-            $("[data-js-stars]").html(stars)
-            $("[data-js-language]").html(language)
-            $("[data-js-link]").attr("href", link)
+    $.ajax({
+        url: getRepDetail,
+        dataType: "json",
+        complete: function(data) {
+            const json = data.responseJSON;
+
+            if (json.message == "Not Found") {
+                console.log("Repositório não encontrado")
+            } else {
+
+                boxRep.addClass("rep--visible")
+                boxRep.removeClass("rep--hidden")
+
+                const name = json.name;
+                const description = json.description;
+                const stars = json.stargazers_count;
+                const language = json.language;
+                const link = json.svn_url;
+
+                $("[data-js-name]").html(name)
+                $("[data-js-desc]").html(description)
+                $("[data-js-stars]").html(stars)
+                $("[data-js-language]").html(language)
+                $("[data-js-link]").attr("href", link)
+            }
         }
-    }
-})
+    })
+
+})();
